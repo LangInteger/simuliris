37c8f6b8f693e638f3c03aaa21b0157e2b4f63c4
Ralf Jung <jung@mpi-sws.org>
2021-01-15 17:50:05 +0100

    remove some generated files

--- .Makefile.coq.d
+++
@@ -1,3 +0,0 @@
-theories/simulation/language.vo theories/simulation/language.glob theories/simulation/language.v.beautified theories/simulation/language.required_vo: theories/simulation/language.v 
-theories/simulation/language.vio: theories/simulation/language.v 
-theories/simulation/language.vos theories/simulation/language.vok theories/simulation/language.required_vos: theories/simulation/language.v 
