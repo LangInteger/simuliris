9838f340e2c8866d1a7e8eaa2d85061f28c0bf5a
Ralf Jung <jung@mpi-sws.org>
2021-06-10 13:03:37 +0200

    move gen_ctx_rel to separate file

--- theories/simplang/simple_inv/adequacy.v
+++ theories/simplang/simple_inv/adequacy.v
@@ -1,12 +1,18 @@
+(** The logical relation implies contextual refinement. *)
+
 From iris.algebra.lib Require Import gset_bij.
 From iris.base_logic.lib Require Import gset_bij.
 
 From simuliris.logic Require Import satisfiable.
 From simuliris.simulation Require Import slsls global_sim.
 From simuliris.simplang Require Import proofmode tactics.
-From simuliris.simplang Require Import gen_adequacy.
+From simuliris.simplang Require Import gen_adequacy behavior wf parallel_subst.
 From simuliris.simplang.simple_inv Require Import inv refl.
 
+(** Instantiate our notion of contextual refinement. *)
+Notation ctx_rel := (gen_ctx_rel expr_head_wf).
+
+(** Whole-program adequacy. *)
 Class simpleGpreS Σ := {
   sbij_pre_heapG :> sheapGpreS Σ;
   sbij_pre_bijG :> heapbijGpreS Σ;
@@ -17,7 +23,7 @@ Global Instance subG_sbijΣ Σ :
   subG simpleΣ Σ → simpleGpreS Σ.
 Proof. solve_inG. Qed.
 
-Lemma adequacy `{!simpleGpreS Σ} p_t p_s :
+Lemma prog_rel_adequacy `{!simpleGpreS Σ} p_t p_s :
   isat (∀ `(simpleGS Σ), prog_rel p_t p_s) →
   beh_rel p_t p_s.
 Proof.
@@ -33,3 +39,11 @@ Proof.
   iSplitR; first done.
   iIntros (??) "$".
 Qed.
+
+(** Adequacy for open terms. *)
+Theorem log_rel_adequacy `{!simpleGpreS Σ} e_t e_s :
+  isat (∀ `(simpleGS Σ), log_rel e_t e_s) →
+  ctx_rel e_t e_s.
+Proof.
+  (* TODO *)
+Abort.
