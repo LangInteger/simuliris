a7b31dd67982dbe35acdd03294e5095ec5880b3f
Ralf Jung <jung@mpi-sws.org>
2024-10-02 17:00:02 +0200

    fix proof for Coq 8.20

--- theories/logic/satisfiable.v
+++ theories/logic/satisfiable.v
@@ -30,7 +30,7 @@ Section satisfiable.
 
     Global Instance sat_equiv: Proper ((≡) ==> iff) sat.
     Proof.
-      intros P Q Heq; split; intros Hsat; eauto using sat_mono, equiv_entails_1_1, equiv_entails_1_2.
+      intros P Q Heq; split; intros Hsat; (eapply sat_mono; last done); eauto using equiv_entails_1_1, equiv_entails_1_2.
     Qed.
 
     Lemma sat_sep P Q: sat (P ∗ Q) → sat P ∧ sat Q.
