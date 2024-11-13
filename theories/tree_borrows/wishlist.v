ea3cc80e8200db6b40c824e846e6db72cab72867
Neven Villani <vanille@crans.org>
2024-11-13 16:11:53 +0100

    Oops, wishlist is actually load-bearing

---
+++ theories/tree_borrows/wishlist.v
@@ -0,0 +1,17 @@
+From iris.proofmode Require Export proofmode.
+From simuliris.tree_borrows Require Export tree_access_laws tag_protected_laws loc_controlled_laws.
+From iris.base_logic.lib Require Import ghost_map.
+From simuliris.base_logic Require Export gen_sim_prog.
+From simuliris.simulation Require Export slsls.
+From simuliris.simulation Require Import lifting.
+From simuliris.tree_borrows Require Import tkmap_view.
+From simuliris.tree_borrows Require Export defs class_instances.
+From simuliris.tree_borrows Require Import steps_progress steps_inv.
+From simuliris.tree_borrows Require Import logical_state inv_accessors.
+From simuliris.tree_borrows.trees_equal Require Import trees_equal_base random_lemmas.
+From iris.prelude Require Import options.
+
+
+
+
+
